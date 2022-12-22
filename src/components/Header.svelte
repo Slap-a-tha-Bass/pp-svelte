46515b737a09e1c87404c87cca9c185fe077dd7c
Slap-a-tha-Bass <cdeloach16@gmail.com>
2022-12-22 10:56:01 -0600

    build: initial commit, setup tailwindcss

---
+++ src/components/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+	import MdWbSunny from 'svelte-icons/md/MdWbSunny.svelte';
+	import IoIosMoon from 'svelte-icons/io/IoIosMoon.svelte';
+	import IoIosMenu from 'svelte-icons/io/IoIosMenu.svelte';
+	import { darkTheme } from '../theme/store';
+
+	const toggleDark = () => ($darkTheme = !$darkTheme);
+	$: dark = $darkTheme;
+</script>
+
+<div
+	class={`relative flex items-center justify-between px-10 py-5 
+        ${dark ? 'bg-slate-700' : 'bg-gray-300'} 
+        ${dark ? 'text-white' : 'text-gray-900'}
+        `}
+>
+	<div class="h-6 w-6">
+		<IoIosMenu />
+	</div>
+	<h1 class="text-3xl font-bold text-center">Pariah Projects</h1>
+	<button class="h-6 w-6" on:click={toggleDark}>
+		{#if dark == true}
+			<MdWbSunny />
+		{:else}
+			<IoIosMoon />
+		{/if}
+	</button>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Stylesheet from '../theme/Stylesheet.svelte';
+	import { darkTheme } from '../theme/store.js';
+	import Header from '../components/Header.svelte';
+
+	$: dark = $darkTheme;
+</script>
+
+<Stylesheet />
+
+<div class:dark>
+	<div class="min-h-screen transition bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white">
+		<Header />
+	</div>
+</div>
