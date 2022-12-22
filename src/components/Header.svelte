1da4db5074bb0479498ce002373973253c55d037
Slap-a-tha-Bass <cdeloach16@gmail.com>
2022-12-22 14:54:57 -0600

    build: homepage content

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,20 +2,30 @@
 	import MdWbSunny from 'svelte-icons/md/MdWbSunny.svelte';
 	import IoIosMoon from 'svelte-icons/io/IoIosMoon.svelte';
 	import IoIosMenu from 'svelte-icons/io/IoIosMenu.svelte';
-	import { darkTheme } from '../theme/store';
+	import MdClose from 'svelte-icons/md/MdClose.svelte';
+	import { darkTheme, menuOpen } from '../theme/store';
 
+	const toggleMenu = () => ($menuOpen = !$menuOpen);
 	const toggleDark = () => ($darkTheme = !$darkTheme);
+
 	$: dark = $darkTheme;
+	$: open = $menuOpen;
 </script>
 
 <div
-	class={`relative flex items-center justify-between px-10 py-5 
+	class={`relative flex items-center justify-between px-10 py-6 
         ${dark ? 'bg-slate-700' : 'bg-gray-300'} 
         ${dark ? 'text-white' : 'text-gray-900'}
         `}
 >
-	<div class="h-6 w-6">
-		<IoIosMenu />
+	<div class="h-8 w-8">
+		<button class="h-6 w-6" on:click={toggleMenu}>
+			{#if open == true}
+				<MdClose />
+			{:else}
+				<IoIosMenu />
+			{/if}
+		</button>
 	</div>
 	<h1 class="text-3xl font-bold text-center">Pariah Projects</h1>
 	<button class="h-6 w-6" on:click={toggleDark}>
@@ -25,4 +35,18 @@
 			<IoIosMoon />
 		{/if}
 	</button>
+	{#if open == true}
+		<div
+			class={`absolute top-20 left-0 w-64 max-h-max z-10 p-10        
+		${dark ? 'bg-slate-700' : 'bg-gray-300'} 
+        ${dark ? 'text-white' : 'text-gray-900'}`}
+		>
+			<ul class="text-lg">
+				<li class="mt-3">Home</li>
+				<li class="mt-3">Projects</li>
+				<li class="mt-3">Blog</li>
+				<li class="mt-3">Contact</li>
+			</ul>
+		</div>
+	{/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,5 +11,62 @@
 <div class:dark>
 	<div class="min-h-screen transition bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white">
 		<Header />
+		<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+			<h1 class="text-4xl font-bold mb-4">Welcome to Pariah Projects!</h1>
+			<p class="text-lg mt-2">
+				We are a team of experienced web designers and developers who are passionate about creating
+				beautiful and functional websites using the latest technologies. Our goal is to help
+				businesses and individuals establish a strong online presence that reflects their unique
+				brand and values.
+			</p>
+		</div>
+		<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+			<h1 class="text-4xl font-bold mb-4">With Pariah Projects, you can expect:</h1>
+			<ul class="list-disc text-lg">
+				<li class="mt-2 ml-4">
+					Custom website design and development that is tailored to your specific needs and goals
+				</li>
+				<li class="mt-2 ml-4">
+					User-friendly and responsive websites that work seamlessly on all devices
+				</li>
+				<li class="mt-2 ml-4">
+					Integration of advanced features and functionality, such as e-commerce, social media
+					integration, and more
+				</li>
+				<li class="mt-2 ml-4">
+					Ongoing support and maintenance to ensure your website stays up-to-date and secure
+				</li>
+			</ul>
+			<p class="text-lg mt-2">
+				Ready to bring your vision to life? Contact us today to schedule a consultation and start
+				building your dream website.
+			</p>
+		</div>
+		<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+			<h1 class="text-4xl font-bold mb-4">Custom Web Design</h1>
+			<div class="text-lg">
+				<p class="mt-2">
+					At Pariah Projects, we believe that every business deserves a website that is as unique
+					and special as they are. That's why we offer custom web design and development services
+					that are tailored to your specific needs and goals.
+				</p>
+
+				<p class="mt-2">
+					Our team of skilled designers and developers will work closely with you to understand your
+					vision and create a website that truly reflects your brand and values. Whether you need a
+					simple brochure website or a more complex e-commerce platform, we have the expertise and
+					experience to make it happen.
+				</p>
+				<p class="mt-2">
+					We also offer a range of additional services, including SEO optimization, social media
+					integration, and ongoing support and maintenance. With Pariah Projects, you can trust that
+					your website will always be at the top of its game.
+				</p>
+				<p class="mt-2">
+					Don't wait any longer to get the website you deserve – contact us today to start building
+					your dream site!
+				</p>
+			</div>
+		</div>
 	</div>
 </div>
